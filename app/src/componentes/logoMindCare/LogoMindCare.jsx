d9dd3ce2ef77e4c05db44fb00ab10d83688cc05c
Daniel-Marini <daniel.marini@al.infnet.edu.br>
2025-10-02 21:38:53 -0300

    Login Page

--- app/src/componentes/logoMindCare/LogoMindCare.jsx
+++ app/src/componentes/logoMindCare/LogoMindCare.jsx
@@ -1,7 +1,7 @@
 import { Brain } from 'lucide-react';
 import "./logoMindCare.css"
 
-export default function LogoMindCare({onClick}) {
-    return <div onClick={onClick} className="logo"><Brain size={30} />
+export default function LogoMindCare({onClick, style}) {
+    return <div onClick={onClick} className={style}><Brain size={30} />
         <h1 className='titulo'>MindCare</h1></div>
 }
