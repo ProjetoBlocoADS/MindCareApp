1e2535c6a2f8966b3e07ee5c5895541ddbe519d7
gabrielvolponii <gabriel.volponi@al.infnet.edu.br>
2025-10-03 21:04:00 -0300

    footer direitos

--- app/src/componentes/footer/Footer.jsx
+++ app/src/componentes/footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./footer.css"
 export default function Footer() {
     return (
         <footer className="footer-container">
-            <p>© 2023 MindCare. All rights reserved.</p>
+            <p>© 2025 MindCare. Todos direitos reservados.</p>
         </footer>
     );
 }
