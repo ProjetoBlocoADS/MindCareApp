a4d97b4855249250d1f0f3c31cd62707870d012c
pedroyamato096 <pedro.yamato@al.infnet.edu.br>
2025-10-30 16:57:39 -0300

    Continuação da implementação dos filtros

--- app/src/pages/homePaciente/components/Filtros.jsx
+++ app/src/pages/homePaciente/components/Filtros.jsx
@@ -1,9 +1,11 @@
 import BotaoFiltrar from "../../../componentes/btn/BotaoFiltrar";
+import ModalFitros from "./ModalFiltros";
 
 export default function Filtros(){
     return(
         <div>
             <BotaoFiltrar />
+            <ModalFitros/>
         </div>
     )
 }
