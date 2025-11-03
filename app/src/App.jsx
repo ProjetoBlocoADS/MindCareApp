4b5e722d21d37aafdd2383c9da71e1d08b2f24e9
jorgejsilva23 <jorge.jsilva@al.infnet.edu.br>
2025-11-02 22:51:05 -0300

    Adicionado componente CadastroPassword Atualizado a pagina de layout Cadastro Adicionado hover no Button e cursor Pointer

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 import Header from "./componentes/header/Header";
 import Login from "./pages/login/Login";
 import Cadastro from "./pages/cadastro/Cadastro";
+import CadastroPassword from "./pages/cadastro/cadastroPassword";
 import Footer from "./componentes/footer/Footer";
 
 function App() {
@@ -18,7 +19,7 @@ function App() {
           </Routes>
         </main>
       </div>
-        <Footer />  
+        <Footer /> 
     </Router>
   );
 }
