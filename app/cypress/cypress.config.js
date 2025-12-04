e1d251c8e23ac5f994bc4086567badd9f7426fc4
gabrielvolponii <gabriel.volponi@al.infnet.edu.br>
2025-12-04 10:47:17 -0300

    removing trash 2

--- app/cypress/cypress.config.js
+++ app/cypress/cypress.config.js
@@ -4,9 +4,7 @@ module.exports = defineConfig({
   e2e: {
 
     defaultCommandTimeout: 10000,
-    responseTimeout: 30000,
-    requestTimeout: 15000,
-    pageLoadTimeout: 60000,
+  
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
